bea65c06b05dc7f7452d64fa31fc76ae47f16c51
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-06 15:23:50 +0200

    update

--- src/test/java/core/TestBoard.java
+++ src/test/java/core/TestBoard.java
@@ -1,7 +1,29 @@
 package core;
 
-/**
- * Created by vpetit on 06/10/14.
- */
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import simpleGame.core.Board;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestBoard {
+
+
+    private Board board;
+
+    @Before
+    public void setUp(){
+        Board board = new Board(2,6,6,6,6);
+    }
+
+    @Test
+    public void testGetSquareContent(){
+
+        assertTrue(true);
+    }
+
 }
