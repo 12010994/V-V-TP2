e0070c91a9a64ce7b90fb7a0dae61c40d82b83c2
amandine <didinem2@gmail.com>
2014-10-06 15:21:46 +0200

    update

--- src/test/java/core/TestPawn.java
+++ src/test/java/core/TestPawn.java
@@ -1,7 +1,44 @@
 package core;
 
-/**
- * Created by vpetit on 06/10/14.
- */
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+
+import simpleGame.core.Board;
+import simpleGame.core.Pawn;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestPawn {
+
+    @Mock
+    private Board board;
+
+    private Pawn pawn;
+
+
+    //Default parameters Pawn
+    private int posY = 4;
+    private int posX = 4;
+    private int gold = 0;
+    private char letter = ' ';
+
+
+    @Before
+    public void init() {
+        board = mock(Board.class);
+        pawn = new Pawn(letter, posX, posY, board);
+    }
+
+    @Test
+    public void test_getSizeY(){
+
+    }
+
 }
