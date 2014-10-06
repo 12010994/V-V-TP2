231bbc460423788e835a9d6ea72e36e378b3fa05
amandine <didinem2@gmail.com>
2014-10-06 15:58:17 +0200

    update TestPawn

--- src/test/java/core/TestPawn.java
+++ src/test/java/core/TestPawn.java
@@ -4,14 +4,19 @@ package core;
 import org.junit.Before;
 import org.junit.Test;
 
+
 import org.mockito.runners.MockitoJUnitRunner;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 
 import simpleGame.core.Board;
+import simpleGame.core.Direction;
 import simpleGame.core.Pawn;
+import simpleGame.exception.OutOfBoardException;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,7 +32,8 @@ public class TestPawn {
     private int posY = 4;
     private int posX = 4;
     private int gold = 0;
-    private char letter = ' ';
+    private char letter = 'm';
+    private boolean isDead = false;
 
 
     @Before
@@ -38,7 +44,47 @@ public class TestPawn {
 
     @Test
     public void test_getSizeY(){
+        assertNotNull(pawn);
+        assertEquals(posY, pawn.getY());
+    }
 
+    @Test
+    public void test_getSizeX(){
+        assertNotNull(pawn);
+        assertEquals(posX, pawn.getX());
+    }
+
+    @Test
+    public void test_letter(){
+        assertNotNull(pawn);
+        assertEquals(letter, pawn.getLetter());
+    }
+
+    @Test
+    public void test_gold(){
+        assertNotNull(pawn);
+        assertEquals(gold, pawn.getGold());
+    }
+
+    @Test
+    public void test_isDead(){
+        assertNotNull(pawn);
+        assertEquals(isDead, pawn.isDead());
+    }
+
+    @Test
+    public void test_movement() throws OutOfBoardException{
+        assertNotNull(pawn);
+        Direction dirDown = Direction.Down;
+
+
+        when(board.getXSize()).thenReturn(posX);
+        when(board.getYSize()).thenReturn(posY);
+
+        pawn.move(dirDown);
+
+        assertEquals(pawn.getX(), posX);
+        assertEquals(pawn.getY(), posY);
     }
 
 }
