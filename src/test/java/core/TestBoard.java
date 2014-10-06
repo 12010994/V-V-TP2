4da233b620f259db0af0aa09c56b9040f0a649c7
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-06 15:57:46 +0200

    update

--- src/test/java/core/TestBoard.java
+++ src/test/java/core/TestBoard.java
@@ -6,24 +6,45 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import simpleGame.core.Board;
+import simpleGame.core.Pawn;
+
+import java.util.ArrayList;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestBoard {
 
+    @Mock
+    private ArrayList<Pawn> pawns = new ArrayList<Pawn>();
 
     private Board board;
 
+    private int ySize;
+    private int xSize;
+    private int xBonusSquare;
+    private int yBonusSquare;
+    private Pawn currentPawn;
+    private int nbPlayer;
+
     @Before
     public void setUp(){
-        Board board = new Board(2,6,6,6,6);
+
+        board = new Board(2,6,6,5,5);
+        for(int i=0; i<nbPlayer; i++){
+            pawns.add(mock(Pawn.class));
+        }
     }
 
     @Test
     public void testGetSquareContent(){
 
-        assertTrue(true);
+        when(pawns.get(0).getLetter()).thenReturn('a');
+        when(pawns.get(0).getX()).thenReturn(2);
+        when(pawns.get(0).getY()).thenReturn(2);
+        assertEquals('a',board.getSquareContent(2,2).getLetter());
+
     }
 
 }
