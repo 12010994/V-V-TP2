9bea48c5dc6576366362a551e981a0f9d6d1f449
vpetit <victor.petit@etudiant.univ-rennes1.fr>
2014-10-08 11:24:26 +0200

    update fix de pmd

--- src/main/java/simpleGame/core/Board.java
+++ src/main/java/simpleGame/core/Board.java
@@ -15,12 +15,12 @@ public class Board {
     /**
      * The number of squares on the x axis.
      */
-    private int xSize;
+    private final int xSize;
 
     /**
      * The number of squares on the y axis.
      */
-    private int ySize;
+    private final int ySize;
 
     /**
      * The Pawns that currently are on the board.
