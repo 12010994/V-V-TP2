f1316cf1680b1473e70d7212e0eac706c14b738d
amandine <didinem2@gmail.com>
2014-10-08 15:51:43 +0200

    add the method maxgold, isbonussquare...

--- src/test/java/core/TestBoard.java
+++ src/test/java/core/TestBoard.java
@@ -23,9 +23,9 @@ public class TestBoard {
     //Default parameters for Board
     private final int ySize = 6;
     private final int xSize = 5;
-    private int xBonusSquare;
-    private int yBonusSquare;
-    private final int numberOfPawns=2;
+    private int xBonusSquare = 6;
+    private int yBonusSquare = 6;
+    private final int numberOfPawns = 2;
     //private Pawn currentPawn;
     //private int nbPlayer = 2;
 
@@ -42,26 +42,26 @@ public class TestBoard {
     }
 
     @Before
-    public void Init(){
+    public void Init() {
 
         board = new Board(numberOfPawns, xSize, ySize, xBonusSquare, yBonusSquare);
     }
     /*
     getXSize : OK
     getYSize : OK
-    getSquareContent
+    getSquareContent : OK
     removePawn : OK
     addPawn ; OK
-    isBonusSquare
+    isBonusSquare : OK
     numberOfPawns : OK
-    maxGold
-    getNextPawn
-    squareContentSprite
-    removeAllPawns
+    maxGold :
+    getNextPawn :
+    squareContentSprite :
+    removeAllPawns : OK
      */
 
     @Test
-    public void test_getXSize(){
+    public void test_getXSize() {
         assertNotNull(board);
 
         assertEquals(xSize, board.getXSize());
@@ -74,22 +74,44 @@ public class TestBoard {
         assertEquals(ySize, board.getYSize());
     }
 
+
     @Test
-    public void test_numberOfPawns(){
+    public void testGetSquareContent() {
+
+        Pawn pawn1 = new Pawn('a', 2, 2, board);
+        board.addPawn(pawn1);
+        if (board.getSquareContent(2, 2) != null) {
+            assertEquals('a', board.getSquareContent(2, 2).getLetter());
+        } else {
+            fail();
+        }
+
+    }
+
+    @Test
+    public void test_numberOfPawns() {
         assertNotNull(board);
 
         assertEquals(numberOfPawns, board.numberOfPawns());
     }
 
     @Test
-    public void test_addPawn(){
+    public void test_addPawn() {
         assertNotNull(board);
 
         assertEquals(numberOfPawns, board.numberOfPawns());
 
-        Pawn pawn1 = new Pawn('l', 5,4, board);
+        Pawn pawn1 = new Pawn('l', 5, 4, board);
         board.addPawn(pawn1);
-        assertEquals(numberOfPawns+1, board.numberOfPawns());
+        assertEquals(numberOfPawns + 1, board.numberOfPawns());
+    }
+
+    @Test
+    public void test_isBonusSquare(){
+        assertNotNull(board);
+
+        assertFalse(board.isBonusSquare(2,2));
+        assertTrue(board.isBonusSquare(6,6));
     }
 
     @Test
@@ -109,6 +131,21 @@ public class TestBoard {
     }
 
     @Test
+    public void test_maxGold(){
+        assertNotNull(board);
+
+        assertEquals(board.maxGold(), 0);
+
+        int maxGold = 3;
+
+        Pawn pawn = mock(Pawn.class);
+        when(pawn.getGold()).thenReturn(maxGold);
+        board.addPawn(pawn);
+        assertEquals(maxGold, board.maxGold());
+        //System.out.println(board.maxGold());
+    }
+
+    @Test
     public void test_removeAllPawns(){
         assertNotNull(board);
 
@@ -123,24 +160,6 @@ public class TestBoard {
         assertEquals(board.numberOfPawns(), 0);
 
     }
-    /*
-    @Test
-    public void testGetSquareContent(){
 
-        when(pawns.get(0).getLetter()).thenReturn('a');
-        when(pawns.get(0).getX()).thenReturn(2);
-        when(pawns.get(0).getY()).thenReturn(2);
-        if(board.getSquareContent(2,2)!=null){
-            assertEquals('a',board.getSquareContent(2,2).getLetter());
-        }else{
-            fail();
-        }
-
-}*/
-
-    @Test
-    public void testIsBonusSquare(){
-
-    }
 
 }
